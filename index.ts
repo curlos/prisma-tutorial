becbf4e9bfca2760da7842c4a2952976185c1d28
Carlos Martinez <curlosmart@gmail.com>
2021-12-06 13:27:28 -0500

    routes created

--- index.ts
+++ index.ts
@@ -7,23 +7,61 @@ app.use(express.json())
 const prisma = new PrismaClient()
 
 app.post('/', async (req: Request, res: Response) => {
-  const { username, password } = req.body
-  const user = await prisma.user.create({
-    data: {
-      username: username,
-      password: password,
-    }
-  })
+  try {
+    const { username, password } = req.body
+    console.log(req.body)
+
+    const user = await prisma.appUser.create({
+      data: {
+        username: username,
+        password: password,
+      }
+    })
+    res.json(user)
+  } catch (err) {
+    res.json({ err, })
+  }
+})
+
+app.post('/createManyUsers', async (req: Request, res: Response) => {
+  try {
+    const { userList } = req.body
+
+    const users = await prisma.appUser.createMany({
+      data: userList,
+    })
+    res.json(users)
+  } catch (err) {
+    res.json({ err, })
+  }
 })
 
 app.get('/', async (req: Request, res: Response) => {
-  const users = await prisma.user.findMany()
-  res.json(users)
+  try {
+    const users = await prisma.appUser.findMany()
+    res.json(users)
+  } catch (err) {
+    res.json({ err, })
+  }
+})
+
+app.get('/:id', async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id
+    const user = await prisma.appUser.findUnique({
+      where: {
+        id: Number(id),
+      },
+    })
+    res.json(user)
+  } catch (err) {
+    res.json({ err, })
+  }
 })
 
 app.put('/', async (req: Request, res: Response) => {
   const { id, username } = req.body
-  const updatedUser = await prisma.user.update({
+  const updatedUser = await prisma.appUser.update({
     where: {
       id: id
     },
@@ -36,13 +74,17 @@ app.put('/', async (req: Request, res: Response) => {
 })
 
 app.delete('/:id', async (req: Request, res: Response) => {
-  const id = Number(req.params.id)
-  const deletedUser = await prisma.user.delete({
-    where: {
-      id: id,
-    },
-  })
-  res.json(deletedUser)
+  try {
+    const id = Number(req.params.id)
+    const deletedUser = await prisma.appUser.delete({
+      where: {
+        id: id,
+      },
+    })
+    res.json(deletedUser)
+  } catch (err) {
+    res.json({ err, })
+  }
 })
 
 app.listen(3001, () => {
